74200be1c3b11b191df32492114ce9a18fd31860
Ken Simon <ninkendo@gmail.com>
2017-07-14 09:01:31 -0700

    Tweaks

--- gopa.zsh
+++ gopa.zsh
@@ -1,18 +1,16 @@
 # gopa is a dumb function to switch the $GOPATH environment variable to various
 # go.* gopaths in ~/dev
 function gopa() {
-  if [[ -z "${1}" ]]
+  if [[ -n "${1}" ]]
   then
-    export GOPATH="${HOME}/dev/go"
-  else
-    export GOPATH="${1}"
+    export GOPATH="${HOME}/dev/${1}"
   fi
   echo $GOPATH
 }
 
 _gopa() {
-  #zstyle ":completion:*:*:gopa:*:*" file-patterns '%p(^-/):other-files'
-  _path_files -/ -W "${HOME}/dev/go.*" 
+  zstyle ":completion:*:*:gopa:*:*" file-patterns 'go.*:directories'
+  _files -g "go." -W "${HOME}/dev" 
 }
 
 compdef _gopa gopa
