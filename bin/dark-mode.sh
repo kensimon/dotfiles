5cd65e7680be9439bca34e71c43916359bf0bc6e
Ken Simon <ken@kensimon.io>
2025-12-17 17:15:07 -0500

    Update wallpapers path in dark-mode script

--- bin/dark-mode.sh
+++ bin/dark-mode.sh
@@ -14,7 +14,7 @@ function enable() {
     gsettings set org.x.apps.portal color-scheme 'prefer-dark'
     gsettings set org.cinnamon.desktop.interface gtk-theme 'Mint-Y-Dark-Aqua'
     gsettings set org.cinnamon.theme name 'Mint-Y-Dark-Aqua'
-    gsettings set org.cinnamon.desktop.background picture-uri 'file:///home/ksimon/Pictures/40lS66E.jpeg'
+    gsettings set org.cinnamon.desktop.background picture-uri "file://${HOME}/Pictures/Wallpapers/dark-mode-wallpaper.jpg"
     gsettings set org.cinnamon.desktop.background picture-options 'centered'
     gsettings set org.cinnamon.desktop.background primary-color '#000000'
     gsettings set org.cinnamon.desktop.background secondary-color '#000000'
@@ -27,7 +27,7 @@ function disable() {
     gsettings set org.x.apps.portal color-scheme 'prefer-light'
     gsettings set org.cinnamon.desktop.interface gtk-theme 'Mint-Y-Aqua'
     gsettings set org.cinnamon.theme name 'Mint-Y-Aqua'
-    gsettings set org.cinnamon.desktop.background picture-uri 'file:///usr/share/backgrounds/linuxmint-tessa/kemsley_kluane.jpg'
+    gsettings set org.cinnamon.desktop.background picture-uri "file://${HOME}/Pictures/Wallpapers/light-mode-wallpaper.jpg"
     gsettings set org.cinnamon.desktop.background picture-options 'zoom'
 }
 
